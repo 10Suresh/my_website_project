9461db6878b7ea08b75282c93865b80c5b9ddaba
suresh kumar singh <121798736+10Suresh@users.noreply.github.com>
2023-09-20 23:09:44 +0530

    All file

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>s.k.singh Software Solution Pvt.Ltd.</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Software Service and software Solution " />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  );
+}
